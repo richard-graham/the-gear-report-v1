8e4dd76ddb4c37c640e34f597e5ffc408f2b719e
richard-graham <richardcolingraham@gmail.com>
2019-04-01 16:56:28 +1300

    signup form posting to firebase db and auth

--- client/reducers/authReducer.js
+++ client/reducers/authReducer.js
@@ -19,6 +19,18 @@ const authReducer = (state = initialState, action) => {
       case 'SIGNOUT_SUCCESS':
       console.log('signout successs');
         return state
+      case 'SIGNUP_SUCCESS':
+      console.log('SIGNUP_SUCCESS');
+        return {
+          ...state,
+          authError: null
+        }
+      case 'SIGNUP_ERROR':
+      console.log('SIGNUP_ERROR');
+        return {
+          ...state,
+          authError: action.err.message
+        }
     default:
       return state
   }
